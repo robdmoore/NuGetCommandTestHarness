051540c3c6001b3501fc6f6a0be3bae1e819a95f
Robert Moore <me@robdmoore.id.au>
2013-04-29 23:58:07 +0800

    Got the app.config copying working

--- NugetCommandTest/NugetPackage/tools/install.ps1
+++ NugetCommandTest/NugetPackage/tools/install.ps1
@@ -7,5 +7,7 @@ $projectPath = Split-Path -Parent $project.FullName
 $toolAppConfig = Join-Path $contentPath "App.config"
 $projAppConfig = Join-Path $projectPath "App.config"
 
-Copy-Item $toolAppConfig $projAppConfig
-$project.ProjectItems.AddFromFile($projAppConfig)
+if (-not (Test-Path $projAppConfig)) {
+	Copy-Item $toolAppConfig $projAppConfig
+	$project.ProjectItems.AddFromFile($projAppConfig)
+}
